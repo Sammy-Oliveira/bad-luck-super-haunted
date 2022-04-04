c9a790fba1770fe629ddc88c546b710cbb6a740a
Sammy-Oliveira <100244290+Sammy-Oliveira@users.noreply.github.com>
2022-04-04 13:54:20 -0700

    Create instruc.js
    basic instruction screen for the game

---
+++ instruc.js
@@ -0,0 +1,10 @@
+import {k} from "./kaboom.js"
+
+//This is the instruction screen!
+//use the left and right arrow keys to go left and right
+//jump using space
+//enter doors by pressing the up arrow key
+//attack enemies for points!
+//but dont let the flashlight hit you, or you'll lose!
+//you can avoid the flashlight by haunting objects!
+//haunt objects by pressing [insert button here]
