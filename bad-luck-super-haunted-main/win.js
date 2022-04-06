c8470f23b02b317be89b64cbe58c22be9eba52ef
Sammy-Oliveira <100244290+Sammy-Oliveira@users.noreply.github.com>
2022-04-06 10:55:10 -0700

    pedit works

--- bad-luck-super-haunted-main/win.js
+++ bad-luck-super-haunted-main/win.js
@@ -1,1 +1,19 @@
 import {k} from "./kaboom.js"
+let win = function(args) {
+    add([
+    text('yay'),
+    origin('center'),
+    pos(width()/2, height()/3),
+    scale(5),
+  ])
+  
+   add([
+    text('score:' + args.score),
+    origin('center'),
+    pos(width()/2, height()/1.5),
+   // scale(5),
+  ])
+    
+  }
+  
+  export {win}
