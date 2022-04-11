620884a224768df68c9c89be4ace6f832bd548e7
Sammy-Oliveira <100244290+Sammy-Oliveira@users.noreply.github.com>
2022-04-11 13:06:04 -0700

    screen change

--- bad-luck-super-haunted-main/kaboom.js
+++ bad-luck-super-haunted-main/kaboom.js
@@ -2,8 +2,8 @@ import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs"
 
 // initialize kaboom context
 export const k = kaboom({
-    width: 320,
-    height: 240,
+    width: 1500,
+    height: 480,
     font: "sinko",
     canvas: document.querySelector("#mycanvas"),
     background: [ 0, 0, 255, ],
